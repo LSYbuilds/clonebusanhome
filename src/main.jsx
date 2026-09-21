92843e6cd6aab5b546b78494e9a293911a67704f
LSYbuilds <130975837+LSYbuilds@users.noreply.github.com>
2026-09-21 14:06:54 +0900

    배포조정중

--- src/main.jsx
+++ src/main.jsx
@@ -9,7 +9,7 @@ const basename = import.meta.env.DEV ? "" : "/clonebusanhome";
 // 일단 이거 페이지부터  따고!
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter basename={baasename}>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </StrictMode>,
