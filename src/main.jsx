ea0018c683b05e263f63aca29a1ead39b92e1a9d
LSYbuilds <130975837+LSYbuilds@users.noreply.github.com>
2026-09-21 13:55:22 +0900

    배포설정중

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 
-const baasename = import.meta.env.DEV ? "" : "/clonebusanhome";
+const basename = import.meta.env.DEV ? "" : "/clonebusanhome";
 
 // 일단 이거 페이지부터  따고!
 createRoot(document.getElementById("root")).render(
